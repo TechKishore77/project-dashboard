63729bfbb8f187c08f30e59ed494b69468e3012d
TechKishore77 <157279900+TechKishore77@users.noreply.github.com>
2024-01-26 12:39:59 +0530

    Update url.constants.ts

--- frontend/dashboard/src/app/constants/url.constants.ts
+++ frontend/dashboard/src/app/constants/url.constants.ts
@@ -6,14 +6,13 @@ import { environment } from "src/environments/environment";
  */
 export const UrlConstants = {
 
-    BASE_URL: environment.baseUrl,
-    AUTH: {
-        LOGIN: '/auth/token',
-        LOGOUT: '/auth/logout',
-        SERVER_URL: 'http://adebackend-env.eba-ugdyzma3.us-east-1.elasticbeanstalk.com'
-        // SERVER_URL: 'http://localhost:8080'
-    },
-    USER: {
-      REGISTER: '/auth/register'
-    }
+  BASE_URL: environment.baseUrl,
+  AUTH: {
+    LOGIN: '/auth/token',
+    LOGOUT: '/auth/logout',
+    SERVER_URL: 'https://project-dashboard-api.onrender.com'
+  },
+  USER: {
+    REGISTER: '/auth/register'
+  }
 };
